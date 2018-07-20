bce32ea9ee77f47478a274f73404e16ca257a5b3
佐藤雄志 <mawile@h164.185.229.10.1017421.vlan.kuins.net>
2018-07-20 15:10:50 +0900

    4.3.3 unify

--- typing.ml
+++ typing.ml
@@ -7,6 +7,41 @@ let err s = raise (Error s)
 (* Type Environment *)
 type tyenv = ty Environment.t
 
+type subst = (tyvar * ty) list
+
+let rec subst_type subst t = match t with 
+    TyInt -> t
+  | TyBool -> t
+  | TyVar tv -> subst_type subst (List.assoc tv subst) 
+                      (* (match subst with
+                          [] -> t
+                        | (tv', t') :: rest -> if tv = tv' then (subst_type subst t') else (subst_type rest t)
+                      ) *)
+  | TyFun (t1, t2) -> TyFun (subst_type subst t1, subst_type subst t2)
+ 
+(* eqs_of_subst : subst -> (ty * ty) list
+型代入を型の等式集合に変換 *)
+let rec eqs_of_subst s = match s with
+    [] -> []
+  | (tv, ty) :: rest -> (TyVar tv, ty) :: (eqs_of_subst rest)
+
+(* subst_eqs: subst -> (ty * ty) list -> (ty * ty) list
+型の等式集合に型代入を適用 *)
+let rec subst_eqs s eqs = match eqs with
+    [] -> []
+  | (t1, t2) :: rest -> (subst_type s t1, subst_type s t2) :: (subst_eqs s rest)
+
+let rec unify l = match l with
+    [] -> []
+  | (t1, t2) :: rest -> if t1 = t2 then unify rest else 
+                              (match (t1, t2) with
+                                   (TyFun(t11, t12), TyFun(t21, t22)) -> unify ((t11, t21) :: (t12, t22) :: rest)
+                                 | (TyVar tv, ty) 
+                                     -> (tv, ty) :: (unify (subst_eqs [(tv, ty)] rest))
+                                    (* -> List.map (subst_type [(tv, t2)]) (unify (List.map (subst_type [(tv, t2)]) rest)) *)
+                                 | (_, _) -> err ("type error") 
+                              )
+  
 let ty_prim op ty1 ty2 = match op with
     Plus -> (match ty1, ty2 with
                  TyInt, TyInt -> TyInt
@@ -44,3 +79,4 @@ let rec ty_exp tyenv = function
 let ty_decl tyenv = function
     Exp e -> ty_exp tyenv e
   | _ -> err ("Not Implemented!")
+  
